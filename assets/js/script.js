5fe1e83415d56558d6100accbd5b1aa17a8d1c7b
Erick Rivera <yomellamoerick.771@gmail.com>
2024-08-22 21:49:31 -0400

    form and footer listos

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,17 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
 $(document).ready(function(){
-  
+  $('form').on('submit', function(event) {
+    const mensajeLimite = 250;
+    var mensaje = $('#exampleTextarea').val();
+
+    if (mensaje.length > mensajeLimite) {
+      alert(`El mensaje no debe superar los ${mensajeLimite} caracteres.`);
+      $('#exampleTextarea')[0].setCustomValidity(`El mensaje no debe superar los ${mensajeLimite} caracteres.`);
+    } else {
+      $('#exampleTextarea')[0].setCustomValidity('');
+    }
+    event.preventDefault();
+  });
+
 });
