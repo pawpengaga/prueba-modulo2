519fc93ddaa68d2157cc5c4136b303aa834b42f6
Erick Rivera <yomellamoerick.771@gmail.com>
2024-08-22 22:19:08 -0400

    cards convertidas en iteracion

---
+++ assets/js/data.js
@@ -0,0 +1,74 @@
+const cards = [
+  {
+    imgSrc: "assets/img/work1.jpg",
+    title: "UpConst (API REST)",
+    description: "Una aplicación fullstack para llevar registro de actividades en el tiempo. En colaboración con desarrolador frontend.",
+    icons: ["devicon-rails-plain colored", "devicon-postgresql-plain colored", "devicon-amazonwebservices-plain colored"],
+    links: [
+      { text: "Probar", url: "https://uptask-one.vercel.app/" },
+      { text: "Ver código", url: "https://github.com/pawpengaga/upconst-the-backend" }
+    ]
+  },
+  {
+    imgSrc: "assets/img/work2.jpg",
+    title: "Object_Oriented_Game",
+    description: "Engine para historias interactivas por consola, aplicando la orientación a objetos.",
+    icons: ["devicon-ruby-plain colored"],
+    links: [
+      { text: "Probar", url: "https://replit.com/@pawpengaga/object-oriented-game#main.rb" },
+      { text: "Ver código", url: "https://github.com/pawpengaga/object-oriented-game" }
+    ]
+  },
+  {
+    imgSrc: "assets/img/work3.jpg",
+    title: "Bündell Att",
+    description: "Aplicación empresarial con capacidad de gestión de usuarios y postulaciones.",
+    icons: ["devicon-rails-plain colored", "devicon-postgresql-plain colored", "devicon-bootstrap-plain colored", "devicon-javascript-plain colored"],
+    links: [
+      { text: "Probar", url: "https://bundelatt.onrender.com/" },
+      { text: "Ver código", url: "https://github.com/pawpengaga/bundelpencils/" }
+    ]
+  },
+  {
+    imgSrc: "assets/img/work4.jpg",
+    title: "REST API Creator/Consumer",
+    description: "API REST que envía información a frontend que la consume.",
+    icons: ["devicon-rails-plain colored", "devicon-postgresql-plain colored", "devicon-javascript-plain colored"],
+    links: [
+      { text: "Probar", url: "https://pawpengaga.github.io/pro-apiconsumer/" },
+      { text: "Ver código", url: "https://github.com/pawpengaga/pro-apiconsumer" }
+    ]
+  },
+  {
+    imgSrc: "assets/img/work5.jpg",
+    title: "Couppon",
+    description: "Funcionalidad de carrito de compras con múltiples parámetros.",
+    icons: ["devicon-html5-plain colored", "devicon-bootstrap-plain colored", "devicon-javascript-plain colored"],
+    links: [
+      { text: "Probar", url: "https://pawpengaga.github.io/web-cuppon-js/" },
+      { text: "Ver código", url: "https://github.com/pawpengaga/web-cuppon-js" }
+    ]
+  },
+  {
+    imgSrc: "assets/img/work6.png",
+    title: "Animations Page",
+    description: "Motion graphics implementado en web.",
+    icons: ["devicon-aftereffects-plain colored", "devicon-illustrator-plain colored", "devicon-javascript-plain colored"],
+    links: [
+      { text: "Probar", url: "https://pawpengaga.github.io/mypageanima/" },
+      { text: "Ver código", url: "https://github.com/pawpengaga/mypageanima" }
+    ]
+  },
+  {
+    imgSrc: "assets/img/work7.jpg",
+    title: "Blockbuster Clone",
+    description: "Aplicación fullstack que obtiene registros a partir de la conexión con la OMBDapi por medio de una librería personalizada.",
+    icons: ["devicon-rails-plain colored", "devicon-postgresql-plain colored", "devicon-ruby-plain colored"],
+    links: [
+      { text: "Probar", url: "https://blockbusterclone.onrender.com/" },
+      { text: "Ver código", url: "https://github.com/pawpengaga/blockbusterclone" }
+    ]
+  }
+];
+
+export default cards;
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,40 @@
+import cards from './data.js';
 // Activacion de tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+function createCards(cards) {
+  const container = document.querySelector('#portfolio .row');
+
+  cards.forEach(card => {
+    const cardCol = document.createElement('div');
+    cardCol.classList.add('col-sm-12', 'col-md-6', 'col-xl-4', 'mb-5');
+    cardCol.innerHTML = `
+      <div class="card">
+        <img src="${card.imgSrc}" class="card-img-top" alt="${card.title}">
+        <div class="card-body">
+          <h4 class="card-title fw-bold">${card.title}</h4>
+          <p class="card-text">${card.description}</p>
+          <div class="d-flex flex-row justify-content-center gap-4 py-2 flex-wrap">
+            ${card.icons.map(icon => `<i class="${icon} colored display-5"></i>`).join('')}
+          </div>
+          <div class="d-flex justify-content-center text-center gap-3 mt-4">
+            <a href="${card.links[0].url}" class="btn light"><i class="fa-solid fa-link"></i> ${card.links[0].text}</a>
+            <a href="${card.links[1].url}" class="btn light"><i class="fa-solid fa-code"></i> ${card.links[1].text}</a>
+          </div>
+        </div>
+      </div>
+    `;
+    container.appendChild(cardCol);
+  });
+}
+
 
 $(document).ready(function(){
   $('form').on('submit', function(event) {
     const mensajeLimite = 250;
     var mensaje = $('#exampleTextarea').val();
-
+    
     if (mensaje.length > mensajeLimite) {
       alert(`El mensaje no debe superar los ${mensajeLimite} caracteres.`);
       $('#exampleTextarea')[0].setCustomValidity(`El mensaje no debe superar los ${mensajeLimite} caracteres.`);
@@ -15,5 +43,6 @@ $(document).ready(function(){
     }
     event.preventDefault();
   });
-
 });
+createCards(cards);
+console.log(cards)
